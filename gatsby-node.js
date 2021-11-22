3a092bd63f21480085f5654cd40405e4683c5637
Ben Edgington <ben@benjaminion.xyz>
2021-11-22 19:50:35 +0000

    Add internal link check to build process

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,19 @@ const path = require(`path`)
 const execSync = require('child_process').execSync;
 
 exports.onPreInit = ({reporter}) => {
+
+  reporter.info("Checking internal links...")
+  try {
+    const out = execSync('bin/links.awk src/book.md src/book.md', {encoding: 'utf8'})
+    if (out !== "") {
+      reporter.warn("Found some bad internal links:")
+      out.split(/\r?\n/).forEach((line, i) => reporter.warn(line))
+    }
+  } catch (err) {
+    reporter.warn("Unable to check internal links:")
+    err.toString().split(/\r?\n/).forEach((line, i) => reporter.warn(line))
+  }
+
   reporter.info("Unpacking book source...")
   try {
     execSync('bin/update.sh')
