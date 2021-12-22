6a1ba12da5f38ba7ecd002535942d50c02974491
Ben Edgington <ben@benjaminion.xyz>
2021-12-22 17:04:53 +0000

    Add noreferrer in footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -24,7 +24,7 @@ const Footer = () => {
 
   return (
     <footer>
-      <p>Created by {meta.author}. {meta.copyright}. Licensed under <a href={meta.licenceUrl} target="_blank" rel="external noopener">{meta.licence}</a>. Commit <a className="githash" href={meta.gitUrl} target="_blank" rel="external noopener">{meta.gitHash}</a>.</p>
+      <p>Created by {meta.author}. {meta.copyright}. Licensed under <a href={meta.licenceUrl} target="_blank" rel="external noreferrer">{meta.licence}</a>. Commit <a className="githash" href={meta.gitUrl} target="_blank" rel="external noreferrer">{meta.gitHash}</a>.</p>
     </footer>
   )
 }
