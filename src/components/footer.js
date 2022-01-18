bfbed3852e0526fc9df16852d24a8b6b7a1584fb
Ben Edgington <ben@benjaminion.xyz>
2022-01-18 12:29:10 +0000

    Update copyright

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,7 +10,6 @@ const Footer = () => {
       site {
         siteMetadata {
           author
-          copyright
           gitHash
           gitUrl
           licenceUrl
@@ -24,7 +23,7 @@ const Footer = () => {
 
   return (
     <footer>
-      <p>Created by {meta.author}. {meta.copyright}. Licensed under <a href={meta.licenceUrl} target="_blank" rel="external noreferrer">{meta.licence}</a>. Commit <a className="githash" href={meta.gitUrl} target="_blank" rel="external noreferrer">{meta.gitHash}</a>.</p>
+      <p>Created by {meta.author}. Licensed under <a href={meta.licenceUrl} target="_blank" rel="external noreferrer">{meta.licence}</a>. Commit <a className="githash" href={meta.gitUrl} target="_blank" rel="external noreferrer">{meta.gitHash}</a>.</p>
     </footer>
   )
 }
