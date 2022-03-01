985468fd0c73f7082c6efee5e5f0aa719c552632
Ben Edgington <ben@benjaminion.xyz>
2022-03-01 13:31:54 +0000

    Make eth2book.info the canonical domain

--- src/html.js
+++ src/html.js
@@ -12,32 +12,32 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <title>Upgrading Ethereum</title>
-        <meta property="og:image" content="https://upgrading-ethereum.info/f/android-icon-192x192.png" />
+        <meta property="og:image" content="https://eth2book.info/f/android-icon-192x192.png" />
         <meta property="og:title" content="Upgrading Ethereum" />
         <meta property="og:description" content="A technical handbook on Ethereum's move to proof of stake and beyond." />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="eth2book.info" />
         <meta name="twitter:creator" content="@benjaminion_xyz" />
-        <meta name="twitter:image" content="https://upgrading-ethereum.info/f/android-icon-192x192.png" />
+        <meta name="twitter:image" content="https://eth2book.info/f/android-icon-192x192.png" />
         <meta name="description" content="A technical handbook on Ethereum's move to proof of stake and beyond." />
-        <link rel="apple-touch-icon" sizes="57x57" href="https://upgrading-ethereum.info/f/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="https://upgrading-ethereum.info/f/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="https://upgrading-ethereum.info/f/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="https://upgrading-ethereum.info/f/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="https://upgrading-ethereum.info/f/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="https://upgrading-ethereum.info/f/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="https://upgrading-ethereum.info/f/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="https://upgrading-ethereum.info/f/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="https://upgrading-ethereum.info/f/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="https://upgrading-ethereum.info/f/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="https://upgrading-ethereum.info/f/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="https://upgrading-ethereum.info/f/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="https://upgrading-ethereum.info/f/favicon-16x16.png" />
-        <link rel="manifest" href="https://upgrading-ethereum.info/f/manifest.json" />
+        <link rel="apple-touch-icon" sizes="57x57" href="https://eth2book.info/f/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="https://eth2book.info/f/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="https://eth2book.info/f/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="https://eth2book.info/f/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="https://eth2book.info/f/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="https://eth2book.info/f/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="https://eth2book.info/f/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="https://eth2book.info/f/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://eth2book.info/f/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="https://eth2book.info/f/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://eth2book.info/f/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="https://eth2book.info/f/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://eth2book.info/f/favicon-16x16.png" />
+        <link rel="manifest" href="https://eth2book.info/f/manifest.json" />
         <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="https://upgrading-ethereum.info/f/ms-icon-144x144.png" />
+        <meta name="msapplication-TileImage" content="https://eth2book.info/f/ms-icon-144x144.png" />
         <meta name="theme-color" content="#ffffff" />
-        <link rel="stylesheet" type="text/css" href="https://upgrading-ethereum.info/fonts/fonts.css" />
+        <link rel="stylesheet" type="text/css" href="https://eth2book.info/fonts/fonts.css" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
