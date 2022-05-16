a29b453f0b8bea9598be87bc0c6413b1923ed558
Ben Edgington <ben@benjaminion.xyz>
2022-05-16 10:00:21 +0100

    Make quote marks consistent

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,74 +4,74 @@ var date = new Date().toISOString().substr(0, 16).replace('T', ' ') + ' UTC';
 
 function getGitHash() {
   try {
-    return execSync('git log -1 --format="%h" 2>/dev/null', {encoding: 'utf8'}).replace(/(\r\n|\n|\r)/,"")
+    return execSync('git log -1 --format="%h" 2>/dev/null', {encoding: 'utf8'}).replace(/(\r\n|\n|\r)/, '')
   } catch(e) {
-    return "unknown"
+    return 'unknown'
   }
 }
 
 module.exports = {
   siteMetadata: {
-    title: "Upgrading Ethereum",
-    description: "A technical handbook on Ethereum's move to proof of stake and beyond",
-    author: "Ben Edgington",
+    title: 'Upgrading Ethereum',
+    description: 'A technical handbook on Ethereum\'s move to proof of stake and beyond',
+    author: 'Ben Edgington',
     gitHash: getGitHash(),
-    gitUrl: "https://github.com/benjaminion/upgrading-ethereum-book",
+    gitUrl: 'https://github.com/benjaminion/upgrading-ethereum-book',
     date: date,
-    licenceUrl: "https://creativecommons.org/licenses/by-sa/4.0/",
-    licence: "CC BY-SA 4.0",
+    licenceUrl: 'https://creativecommons.org/licenses/by-sa/4.0/',
+    licence: 'CC BY-SA 4.0',
   },
-  pathPrefix: `/altair`,
+  pathPrefix: '/altair',
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `markdown-pages`,
+        name: 'markdown-pages',
         path: `${__dirname}/src/md`,
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         gfm: true,
         plugins: [
-          `gatsby-remark-autolink-headers`,
-          `gatsby-remark-numbered-footnotes`,
-          `gatsby-remark-katex`,
+          'gatsby-remark-autolink-headers',
+          'gatsby-remark-numbered-footnotes',
+          'gatsby-remark-katex',
           {
-            resolve: `my-tooltips`,
+            resolve: 'my-tooltips',
             options: {
-              file: `${__dirname}/src/constants.json`
+              file: `${__dirname}/src/constants.json`,
             }
           },
           {
-            resolve: `gatsby-remark-external-links`,
+            resolve: 'gatsby-remark-external-links',
             options: {
-              target: "_blank",
-              rel: "external noopener"
+              target: '_blank',
+              rel: 'external noopener',
             }
           },
           {
-            resolve: `gatsby-remark-prismjs`,
+            resolve: 'gatsby-remark-prismjs',
             options: {
               noInlineHighlight: true,
             },
           },
           {
-            resolve: `gatsby-remark-copy-linked-files`,
+            resolve: 'gatsby-remark-copy-linked-files',
             options: {
-              destinationDir: `images`,
+              destinationDir: 'images',
               ignoreFileExtensions: [],
             },
           },
         ],
       },
     },
-    `gatsby-plugin-catch-links`,
+    'gatsby-plugin-catch-links',
     {
       resolve: 'gatsby-plugin-htaccess',
       options: {
-        ErrorDocument: `ErrorDocument 404 /altair/404.html`,
+        ErrorDocument: 'ErrorDocument 404 /altair/404.html',
       },
     },
     {
