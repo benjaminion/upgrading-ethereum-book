26e383c2ed64ee1c0010f2580ad5cf3e06482d46
Ben Edgington <ben@benjaminion.xyz>
2022-05-27 12:30:31 +0100

    Add details tags

--- src/html.js
+++ src/html.js
@@ -39,7 +39,35 @@ export default function HTML(props) {
         <meta name="theme-color" content="#ffffff" />
         <link rel="stylesheet" type="text/css" href="https://eth2book.info/fonts/fonts.css" />
         {props.headComponents}
-      </head>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+// Open closed details elements for printing
+window.addEventListener('beforeprint', () => {
+  const allDetails = document.body.querySelectorAll('details');
+  for(let i = 0; i < allDetails.length; i++) {
+    if(allDetails[i].open) {
+      allDetails[i].dataset.open = '1';
+    } else {
+      allDetails[i].setAttribute('open', '');
+    }
+  }
+});
+
+// After printing close details elements not opened before
+window.addEventListener('afterprint', () => {
+  const allDetails = document.body.querySelectorAll('details');
+  for(let i = 0; i < allDetails.length; i++) {
+    if(allDetails[i].dataset.open) {
+      allDetails[i].dataset.open = '';
+    } else {
+      allDetails[i].removeAttribute('open');
+    }
+  }
+});
+                  `,
+        }}
+        />
+    </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
         <div
