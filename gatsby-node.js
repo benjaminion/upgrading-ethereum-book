5891a5df41a6b228c795afad747e435364217e45
Ben Edgington <ben@benjaminion.xyz>
2022-06-23 12:18:22 +0100

    Simplify prebuild check workflow

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,11 @@
 const path = require('path')
-const doPreBuild = require('./bin/build/prebuild')
+const prebuild = require('./bin/build/prebuild')
 
 // Set up a hook to pre-process the source file into split files, and perform various
 // checking and linting operations prior to building.
 exports.onPreInit = ({ reporter }) => {
   try {
-    doPreBuild(reporter)
+    prebuild.runChecks(reporter)
   } catch (err) {
     reporter.panicOnBuild('Could not run pre-build tasks,', err)
   }
