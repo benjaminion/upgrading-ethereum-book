cc91cca337f37ca2eaeae5882ab38c4b8d18c915
Ben Edgington <ben@benjaminion.xyz>
2022-08-08 17:43:20 +0100

    Implement footnote tooltips

--- static/local.js
+++ static/local.js
@@ -3,8 +3,8 @@
 // Open closed details elements for printing
 window.addEventListener('beforeprint', () => {
   const allDetails = document.body.querySelectorAll('details');
-  for(let i = 0; i < allDetails.length; i++) {
-    if(allDetails[i].open) {
+  for (let i = 0; i < allDetails.length; i++) {
+    if (allDetails[i].open) {
       allDetails[i].dataset.open = '1';
     } else {
       allDetails[i].setAttribute('open', '');
@@ -15,11 +15,34 @@ window.addEventListener('beforeprint', () => {
 // After printing close details elements not opened before
 window.addEventListener('afterprint', () => {
   const allDetails = document.body.querySelectorAll('details');
-  for(let i = 0; i < allDetails.length; i++) {
-    if(allDetails[i].dataset.open) {
+  for (let i = 0; i < allDetails.length; i++) {
+    if (allDetails[i].dataset.open) {
       allDetails[i].dataset.open = '';
     } else {
       allDetails[i].removeAttribute('open');
     }
   }
 });
+
+// --- Show tooltips for footnotes ---
+
+window.addEventListener('load', function () {
+
+  const fns = document.body.querySelectorAll('.footnote-ref');
+  for (let i = 0; i < fns.length; i++) {
+
+    // Append new span elements to all footnote links to hold the tooltip
+    let fnSpan = document.createElement('span');
+    fnSpan.className = 'fn-span';
+    fns[i].parentElement.append(fnSpan);
+
+    fns[i].addEventListener("mouseover", function(event) {
+      let fnTooltip = event.target.parentElement.querySelector('span.fn-span');
+      if (fnTooltip.innerHTML.trim() == '') {
+        let fnSelector = event.target.attributes.href.textContent;
+        fnTooltip.innerHTML = document.body.querySelector(fnSelector).innerHTML;
+      }
+    }, false);
+
+  }
+}, false);
