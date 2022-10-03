27b21f5632ff298e4853c93daeb1f2d4ae33e3ae
Ben Edgington <ben@benjaminion.xyz>
2022-10-03 10:50:38 +0100

    Add banner files

---
+++ src/components/banner.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "./banner.css"
+
+const Banner = () => {
+
+  return (
+    <div id="banner">
+      <p>You are viewing an outdated version of the text (Altair). The most recent version of the book is <a href="/latest">here</a>.</p>
+    </div>
+  )
+}
+
+export default Banner
