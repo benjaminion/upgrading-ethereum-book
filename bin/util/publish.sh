dd368d8b1a6afdcdf59845ffda884a502c56be1e
Ben Edgington <ben@benjaminion.xyz>
2022-10-12 10:47:58 +0100

    Add publish script

---
+++ bin/util/publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+version=bellatrix
+
+# Set the host variable
+source ../priv/server.sh
+
+gatsby clean
+gatsby build --prefix-paths
+
+if [ $? -ne 0 ]
+then
+    echo "gatsby build failed"
+    exit 1
+fi
+
+tar zcf - public | ssh $host tar zxfC - eth2book
+ssh $host eth2book/install_$version.sh
