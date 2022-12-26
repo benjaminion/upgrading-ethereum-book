7172ddfa04891ef9f7c93bf20d4b32da18be8574
Ben Edgington <ben@benjaminion.xyz>
2022-12-26 19:46:28 +0000

    Add title elements to SVGs based on alt text

--- plugins/my-svg-embed/index.js
+++ plugins/my-svg-embed/index.js
@@ -1,10 +1,53 @@
 const visit = require('unist-util-visit')
-const { optimize } = require('svgo');
+const svgo = require('svgo')
 const fs = require('fs')
-const path = require('path');
+const path = require('path')
 const getHashDigest = require('loader-utils/lib/getHashDigest')
 
 // Inline SVG files into the Markdown AST
+
+// SVGO doesn't really support adding elements, and the API changes.
+// The below is based on code from the "reusePaths" plugin.
+const addTitle = {
+  name: "addTitle",
+  type: "visitor",
+  active: true,
+  fn: (ast, params) => {
+    return {
+      element: {
+        exit: (node, parentNode) => {
+          if (node.name === "svg" && parentNode.type === "root") {
+            const hasTitle = node.children.some(
+              (child) => child.type === "element" && child.name === "title"
+            )
+            if (!hasTitle) {
+              const titleElement = {
+                type: 'element',
+                name: 'title',
+                attributes: {},
+                children: [],
+              }
+              Object.defineProperty(titleElement, 'parentNode', {
+                writable: true,
+                value: node,
+              });
+              const titleContents = {
+                type: "text",
+                value: params.titleText,
+              }
+              Object.defineProperty(titleContents, 'parentNode', {
+                writable: true,
+                value: titleElement,
+              });
+              titleElement.children.push(titleContents)
+              node.children.unshift(titleElement);
+            }
+          }
+        },
+      },
+    }
+  },
+}
 
 // See https://www.npmjs.com/package/svgo
 const plugins = [
@@ -17,20 +60,41 @@ const plugins = [
   },
 ]
 
+var addTitleSettings =  {
+  name: addTitle.name,
+  type: addTitle.type,
+  active: addTitle.active,
+  fn: addTitle.fn,
+  params: undefined,
+}
+
 module.exports = ({ markdownAST }, pluginOptions) => {
   try {
     visit(markdownAST, "paragraph", node => {
       if (node.children[0].type == 'image') {
         image = node.children[0]
+
         if (image.url.endsWith('.svg')) {
-          const data = fs.readFileSync(pluginOptions.directory + image.url, 'utf8')
+          const originalSvg = fs.readFileSync(pluginOptions.directory + image.url, 'utf8')
           const basename = path.basename(image.url, '.svg')
+
           // We need to distinguish multiple SVGs on the same page by using "prefixIds"
           const digest = getHashDigest(basename, 'md5', 'base52').substring(0,4)
-          const svg = optimize(data, {path: digest, plugins: plugins})
+
+          // Configure our custom plugin for SVGO that adds title element
+          addTitleSettings['params'] = {titleText: image.alt}
+          const svg = svgo.optimize(
+            originalSvg,
+            {
+              path: digest,
+              plugins: plugins.concat([addTitleSettings])
+            }
+          )
+
+          // Modify the current node in-place
           node.type = 'html'
           node.value = '<div id="' + basename + '">' + svg.data + '</div>'
-          node.children = undefined
+          node.children = []
         }
       }
     })
