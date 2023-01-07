297d69e4a3a0560cae599816fb59d2cfd051810e
Ben Edgington <ben@benjaminion.xyz>
2023-01-07 22:09:44 +0000

    Fix footnote id attributes

--- src/components/footnote-tooltips.js
+++ src/components/footnote-tooltips.js
@@ -15,26 +15,26 @@ const stripIds = (node) => {
 class FootnoteTooltips extends Component {
 
   componentDidMount() {
-    const fns = document.body.querySelectorAll('.footnote-ref')
-    for (let i = 0; i < fns.length; i++) {
 
-      // For finding the footnote contents
-      const fnSelector = fns[i].attributes.href.textContent
+    document.body.querySelectorAll('.footnote-ref').forEach( (fn) => {
 
-      // Append new span elements to all footnote links to hold the tooltip
-      if (fns[i].querySelector('span.fn-span') === null) {
+      if (fn.querySelector('span.fn-span') === null) {
 
-        const footnote = document.body.querySelector(fnSelector)
+        // Find and clone the footnote
+        const toolTip = document.body.querySelector(fn.attributes.href.textContent).cloneNode(true)
 
-        // Remove any ID attributes from the copied HTML to preserve uniqueness
-        stripIds(footnote)
+        // Avoid introducing duplicate ids into our page
+        stripIds(toolTip)
 
+        // TODO: remove the backlink: a.footnote-backref
+
+        // Append a new span element to the footnote link to hold the tooltip
         const fnSpan = document.createElement('span')
         fnSpan.className = 'fn-span'
-        fnSpan.innerHTML = footnote.innerHTML
-        fns[i].parentElement.append(fnSpan)
+        fnSpan.appendChild(toolTip)
+        fn.parentElement.append(fnSpan)
       }
-    }
+    })
   }
 
   render() {
