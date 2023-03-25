325d8ae610ef4633fc0bc9e2ba182dd6cdfcd288
Ben Edgington <ben@benjaminion.xyz>
2023-03-25 22:13:26 +0000

    Improve footnote tooltips

--- src/components/footnote-tooltips.js
+++ src/components/footnote-tooltips.js
@@ -2,15 +2,6 @@ import { Component } from 'react'
 
 import '../css/footnote-tooltips.css'
 
-const stripIds = (node) => {
-  if (node.removeAttribute !== undefined) {
-    node.removeAttribute('id')
-  }
-  if (node.children !== undefined) {
-    Array.from(node.children).forEach((node) => stripIds(node))
-  }
-}
-
 // Modify the page to duplicate any footnotes into tooltips.
 class FootnoteTooltips extends Component {
 
@@ -20,19 +11,39 @@ class FootnoteTooltips extends Component {
 
       if (fn.parentElement.querySelector('span.fn-span') === null) {
 
-        // Find and clone the footnote
-        const toolTip = document.body.querySelector(fn.attributes.href.textContent).cloneNode(true)
+        // Find the footnote
+        const toolTip = document.body.querySelector(fn.attributes.href.textContent)
 
-        // Avoid introducing duplicate ids into our page
-        stripIds(toolTip)
-
-        // TODO: remove the backlink: a.footnote-backref
-
-        // Append a new span element to the footnote link to hold the tooltip
+        // Create a new span element to hold the tooltip
         const fnSpan = document.createElement('span')
         fnSpan.className = 'fn-span'
-        fnSpan.appendChild(toolTip)
+        for (let i = 0; i < toolTip.childNodes.length; i++) {
+          fnSpan.appendChild(toolTip.childNodes[i].cloneNode(true))
+        }
+
+        // Remove any backlinks (there should be exactly one)
+        const backlinks = fnSpan.getElementsByClassName('footnote-backref')
+        for (let i = 0; i < backlinks.length; i++) {
+          backlinks[i].parentElement.removeChild(backlinks[i])
+        }
+
+        // Ensure the tooltip doesn't overflow the main content area
+        fnSpan.addEventListener("transitionstart", (event) => {
+          const span = event.target
+          const padding = 16;
+          const mainRect = document.getElementById('main-content').getBoundingClientRect()
+          const fnRect = fn.getBoundingClientRect()
+          const spanRect = span.getBoundingClientRect()
+          const spanWidth = spanRect.right - spanRect.left
+          var left = (fnRect.right + fnRect.left - spanWidth) / 2
+          left = Math.max(left, mainRect.left + padding)
+          left = Math.min(left, mainRect.right - spanWidth - padding)
+          const zeroPos = fnRect.right
+          span.style.left=`${left - zeroPos}px`
+        })
+
         fn.parentElement.append(fnSpan)
+
       }
     })
   }
