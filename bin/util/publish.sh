341eb134d2730100e57df8c254e6e6d78d1f3325
Ben Edgington <ben@benjaminion.xyz>
2023-04-14 11:16:25 +0100

    Add PDF to publishing script

--- bin/util/publish.sh
+++ bin/util/publish.sh
@@ -6,24 +6,41 @@ echo
 echo "*** Publishing to path $version ***"
 echo
 
+dir=$(dirname "$0")/../..
+
 # Set the host variable
-source $(dirname "$0")/../priv/server.sh
+source $dir/bin/priv/server.sh
 
 wait_for_input () {
-    echo "Press [ENTER] to continue"
     read -s < /dev/tty
 }
+
+echo "*** Building site..."
 
 gatsby clean
 gatsby build --prefix-paths
 
 if [ $? -ne 0 ]
 then
-    echo "gatsby build failed"
+    echo "Error: gatsby build failed"
     exit 1
 fi
 
+echo "*** Building PDF..."
+$dir/bin/pdf/make_pdf $dir/src/book.md
+
+if [ $? -ne 0 ]
+then
+    echo "Error: pdf build failed"
+    exit 1
+fi
+
+mv book.pdf $dir/public/
+
+echo "*** Ready to upload - press [ENTER] to continue"
 wait_for_input
 tar zcf - public | ssh $host tar zxfC - eth2book
+
+echo "*** Ready to install - press [ENTER] to continue"
 wait_for_input
 ssh $host eth2book/install_$version.sh
