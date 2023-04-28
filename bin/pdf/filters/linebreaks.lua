ccbe2672e67e22b7b88ef997a54d8386d9543f40
Ben Edgington <ben@benjaminion.xyz>
2023-04-28 16:40:53 +0100

    Improve footnotes in PDF

---
+++ bin/pdf/filters/linebreaks.lua
@@ -0,0 +1,11 @@
+local logging = require 'logging'
+
+-- Linebreaks filter
+--
+-- Converts html <br/> into pandoc linebreaks
+
+function RawInline (el)
+  if el.format == 'html' and el.text == '<br/>' then
+    return pandoc.LineBreak()
+  end
+end
