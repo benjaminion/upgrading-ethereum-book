622cde691336ecce066fabfbb7728692304a8f96
Ben Edgington <ben@benjaminion.xyz>
2023-05-09 16:37:14 +0100

    Process <wbr/> as a soft line break

--- bin/pdf/filters/linebreaks.lua
+++ bin/pdf/filters/linebreaks.lua
@@ -1,9 +1,17 @@
 -- Linebreaks filter
 --
--- Converts html <br/> into pandoc linebreaks
+-- Converts
+--   - html <br/> into pandoc linebreaks, and
+--   - html <wbr/> into latex suggested linebreaks.
 
 function RawInline (el)
-  if el.format == 'html' and el.text == '<br/>' then
-    return pandoc.LineBreak()
-  end
+   if el.format == 'html' then
+      if el.text == '<br/>' then
+         return pandoc.LineBreak()
+      end
+      if el.text == '<wbr/>' then
+         return pandoc.RawInline('latex', '\\linebreak[0]')
+      end
+   end
+   return el
 end
