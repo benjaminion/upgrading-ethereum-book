e8a735fa8fbb851266b3a3ac696b4f755df5e1dc
Ben Edgington <ben@benjaminion.xyz>
2023-05-18 16:51:05 +0100

    Add byte count to stats

--- bin/util/stats.sh
+++ bin/util/stats.sh
@@ -5,6 +5,7 @@ here=$(dirname "$0")
 src=$here/../../src
 pdf=$here/../../book.pdf
 
+bytes=$(cat $src/book.md | wc -c)
 footnotes=$(grep '^\[^.*\]:' $src/book.md | wc -l)
 words=$($here/../build/checks/spellcheck_prep.pl $src/book.md | wc -w)
 pages=$(pdfinfo $pdf | grep '^Pages' | awk '{print $2}')
@@ -13,6 +14,7 @@ internal=$(cat $src/book.md | grep -Pho '\(\K/[^)]+' | wc -l)
 charts=$(ls $src/images/charts/*.svg | wc -l)
 diagrams=$(ls $src/images/diagrams/*.svg | wc -l)
 
+echo Bytes: $bytes
 echo Words: $words
 echo Pages: $pages
 echo Images: $((charts + diagrams))
