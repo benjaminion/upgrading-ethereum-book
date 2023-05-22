af09371c147d642b77f163c5d9d2069f2e9e833a
Ben Edgington <ben@benjaminion.xyz>
2023-05-22 09:03:10 +0100

    Improve links from the deprecation banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import "../css/banner.css"
 
@@ -6,15 +7,44 @@ const Banner = ({path}) => {
 
   const addBanner = false
 
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              path
+              hide
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const pages = data.allMarkdownRemark.edges
+  const hide = pages.filter(page => page.node.frontmatter.path === path)[0].node.frontmatter.hide
+
   if (addBanner) {
 
-    const newPage = "/latest" + path
+    if (hide) {
 
-    return (
-      <div id="banner">
-        <p>This version is old (Capella). The latest book is <a href="/latest/">here</a>, and the updated page may be <a href={newPage}>here</a>.</p>
-      </div>  
-    )
+      return (
+        <div id="banner">
+          <p>This version is old (Capella). The latest book is <a href="/latest/">here</a>.</p>
+        </div>  
+      )
+
+    } else {
+
+      const newPage = "/latest" + path
+      return (
+        <div id="banner">
+          <p>This version is old (Capella). The latest book is <a href="/latest/">here</a>. An updated page may be <a href={newPage}>here</a>.</p>
+        </div>  
+      )
+
+    }
 
   } else {
 
