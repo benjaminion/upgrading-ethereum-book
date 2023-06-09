324c1233f7067055a894e5b91c31aae2a75b795e
Ben Edgington <ben@benjaminion.xyz>
2023-06-09 09:09:21 +0100

    Patch node modules when publishing

--- bin/util/publish.sh
+++ bin/util/publish.sh
@@ -2,45 +2,52 @@
 
 version=altair
 
-echo
-echo "*** Publishing to path $version ***"
-echo
-
-dir=$(dirname "$0")/../..
-
-# Set the host variable
-source $dir/bin/priv/server.sh
-
 wait_for_input () {
     read -s < /dev/tty
 }
 
+was_it_ok () {
+    if [ $1 -ne 0 ]
+    then
+        echo "Exiting: $2 failed."
+        exit 1
+    fi
+}
+
+echo
+echo "*** Publishing to path $version ***"
+
+cd $(dirname "$0")/../..
+
+# Set the host variable
+source bin/priv/server.sh
+
+echo
+echo "*** Patching node modules ***"
+
+npx patch-package --error-on-fail
+was_it_ok $? "patch-package"
+
+echo
 echo "*** Building site..."
 
 gatsby clean
 gatsby build --prefix-paths
+was_it_ok $? "gatsby build"
 
-if [ $? -ne 0 ]
-then
-    echo "Error: gatsby build failed"
-    exit 1
-fi
-
+echo
 echo "*** Building PDF..."
-$dir/bin/pdf/make_pdf $dir/src/book.md
+bin/pdf/make_pdf src/book.md
+was_it_ok $? "make_pdf"
 
-if [ $? -ne 0 ]
-then
-    echo "Error: pdf build failed"
-    exit 1
-fi
+mv book.pdf public/
 
-mv book.pdf $dir/public/
-
+echo
 echo "*** Ready to upload - press [ENTER] to continue"
 wait_for_input
 tar zcf - public | ssh $host tar zxfC - eth2book
 
+echo
 echo "*** Ready to install - press [ENTER] to continue"
 wait_for_input
 ssh $host eth2book/install_$version.sh
