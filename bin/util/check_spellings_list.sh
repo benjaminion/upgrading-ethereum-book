5004d495d4ec01a228f9e5e87ba9f45d03266209
Ben Edgington <ben@benjaminion.xyz>
2023-06-26 17:42:09 +0100

    Rework spelling checks

--- bin/util/check_spellings_list.sh
+++ bin/util/check_spellings_list.sh
@@ -7,7 +7,7 @@ export LC_ALL=C.UTF-8
 here=$(dirname "$0")
 check=$here/../build/checks/spellcheck.sh
 source=$here/../../src/book.md
-wordlist=$here/../../src/spellings.txt
+wordlist=$here/../../src/spellings.en.pws
 
 # Pre-requisite is to pass a normal spell check (no words missing)
 output=$($check $source $wordlist)
@@ -20,8 +20,8 @@ output=$($check $source $wordlist)
 # Now spell check against an empty list and compare (no extra words)
 missing=$(mktemp)
 
-$check $source /dev/null | awk '{print $3}' | sed "s/'s$//" | sort -u > $missing
+$here/make_spellings_list.sh > $missing
 
-diff $missing $wordlist
+tail -n +2 $wordlist | diff $missing -
 
 rm -f $missing
