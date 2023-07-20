346b83bc7cc2f7cc84ee1e2b63385922e7465ead
Ben Edgington <ben@benjaminion.xyz>
2023-07-20 14:23:37 +0100

    Fix word count stats

--- bin/util/stats.sh
+++ bin/util/stats.sh
@@ -7,7 +7,7 @@ pdf=$here/../../book.pdf
 
 bytes=$(cat $src/book.md | wc -c)
 footnotes=$(grep '^\[^.*\]:' $src/book.md | wc -l)
-words=$($here/../build/checks/spellcheck_prep.pl $src/book.md | wc -w)
+words=$($here/../build/checks/spellcheck_prep.pl $src/book.md | sed 's/^^ //' | wc -w)
 pages=$(pdfinfo $pdf | grep '^Pages' | awk '{print $2}')
 external=$(cat $src/book.md | grep -Pho '\(\Khttp[^)]+' | sed 's/#.*$//g' | sort -u | wc -l)
 internal=$(cat $src/book.md | grep -Pho '\(\K/[^)]+' | wc -l)
