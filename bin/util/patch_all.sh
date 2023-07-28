c2e1fc64697ccea3a44d452cd8c6b04c1f588c06
Ben Edgington <ben@benjaminion.xyz>
2023-07-28 15:09:06 +0100

    Rename update branches script

--- bin/util/patch_all.sh
+++ bin/util/patch_all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#
 # Apply a git commit to all branches
+#
 
 branches='altair bellatrix capella'
 
@@ -8,6 +10,9 @@ if [ $# -eq 0 ]; then
     echo "Usage: $0 <commit>"
     exit 1
 fi
+
+# Save the starting branch so we can return to it later
+start=$(git branch --show-current)
 
 for branch in $branches;
 do
@@ -20,3 +25,5 @@ do
         git cherry-pick --abort
     fi
 done
+
+git switch $start
