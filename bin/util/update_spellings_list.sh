7acd14f8feb256fca1bdf88a347e59a0f0a4c8b5
Ben Edgington <ben@benjaminion.xyz>
2025-03-19 11:26:29 +0000

    Add new spellcheck utility

---
+++ bin/util/update_spellings_list.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update the spellings list with all the current "mis-spellings"
+
+here=$(dirname "$0")
+wordlist=$here/../../src/spellings.en.pws
+newlist=$(mktemp)
+
+$here/make_spellings_list.sh > $newlist
+
+count=$(cat $newlist | wc -l)
+
+# Add header line
+echo "personal_ws-1.1 en $count utf-8" > $wordlist
+cat $newlist >> $wordlist
+
+rm $newlist
