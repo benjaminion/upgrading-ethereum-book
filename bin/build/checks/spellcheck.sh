e335244713d475937bece3764581aa0c7709b87f
Ben Edgington <ben@benjaminion.xyz>
2025-04-17 11:13:44 +0100

    Add dictionaries to repo for easier maintenance

--- bin/build/checks/spellcheck.sh
+++ bin/build/checks/spellcheck.sh
@@ -6,8 +6,9 @@
 # with the spellcheck_prep.pl script.
 
 export LANG=en_GB.UTF-8
+here=$(cd $(dirname "$0") && pwd)
 
-$(dirname "$0")/spellcheck_prep.pl $1 \
-    | aspell --home-dir . -p $2 --dont-suggest pipe -d en_GB-ise-w_accents \
+$here/spellcheck_prep.pl $1 \
+    | aspell --home-dir . -p $2 --dont-suggest pipe --dict-dir=$here/../dicts -d en_GB-ise-w_accents \
     | tail -n +2 \
     | awk 'BEGIN {n=1} /^$/{n++} /^..+$/{print "Line " n ": "$2}'
