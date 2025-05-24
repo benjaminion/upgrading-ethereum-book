5e398ed87843e19dcd8d5bd60db9681a75e75a49
Ben Edgington <ben@benjaminion.xyz>
2025-05-24 20:48:15 +0100

    Use eslint

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import css from '@eslint/css';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    rules: {
+      'no-unused-vars': [
+        'error',
+        {
+          caughtErrors: 'none',
+        },
+      ],
+    },
+  },
+]);
