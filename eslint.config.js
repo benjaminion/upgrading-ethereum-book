b7bc0cad9adb98b6b8980961cd45da5039ec6f90
Ben Edgington <ben@benjaminion.xyz>
2025-05-27 15:56:09 +0100

    Apply eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
+import eslintPluginAstro from 'eslint-plugin-astro';
 
 export default defineConfig([
   {
@@ -22,4 +23,5 @@ export default defineConfig([
       ],
     },
   },
+  ...eslintPluginAstro.configs.recommended,
 ]);
