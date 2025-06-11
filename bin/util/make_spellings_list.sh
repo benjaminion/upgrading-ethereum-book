b442bf8ef7a32b1b0dc124d7c942329a1f8fea5e
Ben Edgington <ben@benjaminion.xyz>
2025-06-11 21:33:11 +0100

    Add explanatory comment

--- bin/util/make_spellings_list.sh
+++ bin/util/make_spellings_list.sh
@@ -8,4 +8,6 @@ here=$(dirname "$0")
 check=$here/../build/checks/spellcheck.sh
 source=$here/../../src/book.md
 
+# aspell will also match 'randao' with both 'Randao' and 'RANDAO' and I don't know how
+# to stop it. The following emulates this to avoid superfluous entries in the list.
 $check $source /dev/null | awk '{print $3}' | sort -fr | awk '!seen[tolower($0)]++' | tac
