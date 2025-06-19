3d29b3eb68e0ac22a009fc7b11b3656399355440
Ben Edgington <ben@benjaminion.xyz>
2025-06-19 20:12:50 +0100

    PDF: Simplify summary box handling

--- bin/pdf/filters/summaries.lua
+++ bin/pdf/filters/summaries.lua
@@ -2,18 +2,9 @@
 --
 -- Find summary boxes in the text and wrap them in a `summarybox` environment.
 
-local function is_summary(div)
-   for _, class in pairs(div.classes) do
-      if class == 'summary' then
-         return true
-      end
-   end
-   return false
-end
-
 function Div (div)
 
-   if is_summary(div) then
+   if div.classes:includes('summary') then
       return {
          pandoc.RawInline('latex', '\\begin{summarybox}'),
          div,
